a438ad8927018cd53fb6230acb0fc44b91b1382d
Peter Souter <p.souter@kainos.com>
2014-07-31 23:46:07 +0100

    A few tweaks from the human_log plugin

--- callback_plugins/html_logs.py
+++ callback_plugins/html_logs.py
@@ -1,9 +1,10 @@
 # Adapted from https://github.com/ansible/ansible/blob/devel/plugins/callbacks/log_plays.py
+# And https://gist.github.com/cliffano/9868180
 
 import os
 import time
 import json
-import datetime
+from datetime import datetime
 
 TIME_FORMAT="%b %d %Y %H:%M:%S"
 MSG_FORMAT="%(now)s - %(category)s - %(data)s\n\n"
@@ -14,87 +15,91 @@ todaystr = today.isoformat()
 if not os.path.exists("/var/log/ansible/hosts/html"):
     os.makedirs("/var/log/ansible/hosts/html")
 
-def log(host, category, data):
-    if type(data) == dict:
-        if 'verbose_override' in data:
-            # avoid logging extraneous data from facts
-            data = 'omitted'
-        else:
-            data = data.copy()
-            invocation = data.pop('invocation', None)
-            data = json.dumps(data)
-            if invocation is not None:
-                data = json.dumps(invocation) + " => %s " % data
+datenow = datetime.now()
+datenow = datenow.strftime('%Y-%m-%d')
 
-    path = os.path.join("/var/log/ansible/hosts/html/"+host+todaystr+".html")
-    now = time.strftime(TIME_FORMAT, time.localtime())
-    fd = open(path, "a")
+FIELDS = ['cmd', 'command', 'start', 'end', 'delta', 'msg', 'stdout', 'stderr']
 
-    fd.write(MSG_FORMAT % dict(now=now, category=category, data=data))
-
-    fd.close()
+def human_log(res, host):
+  filnename = ("/var/log/ansible/hosts/html/"+host+datenow+".html")
+  path = os.path.join(filnename)
+  fd = open(path, "a")
+  fd.write("<h1>"+host+" "+datenow+"</h1>")
+  fd.close()
+  if type(res) == type(dict()):
+    for field in FIELDS:
+      if field in res.keys():
+        html_string = '<b>{0}</b>: <p>{1}</p>'.format(field, res[field])
+        path = os.path.join(filnename)
+        fd = open(path, "a")
+        fd.write(html_string)
+        fd.close()
+  print("HTML log created at "+filnename)
 
 class CallbackModule(object):
-    """
-    logs playbook results, per host, in /var/log/ansible/hosts
-    """
+  """
+    logs playbook results, per host, as a basic html file in /var/log/ansible/hosts/html/
+  """
 
-    def on_any(self, *args, **kwargs):
-        pass
+  def on_any(self, *args, **kwargs):
+    pass
 
-    def runner_on_failed(self, host, res, ignore_errors=False):
-        log(host, 'FAILED', res)
+  def runner_on_failed(self, host, res, ignore_errors=False):
+    human_log(res, host)
 
-    def runner_on_ok(self, host, res):
-        log(host, 'OK', res)
+  def runner_on_ok(self, host, res):
+    human_log(res, host)
 
-    def runner_on_skipped(self, host, item=None):
-        log(host, 'SKIPPED', '...')
+  def runner_on_error(self, host, msg):
+    pass
 
-    def runner_on_unreachable(self, host, res):
-        log(host, 'UNREACHABLE', res)
+  def runner_on_skipped(self, host, item=None):
+    pass
 
-    def runner_on_no_hosts(self):
-        pass
+  def runner_on_unreachable(self, host, res):
+    human_log(res, host)
 
-    def runner_on_async_poll(self, host, res, jid, clock):
-        pass
+  def runner_on_no_hosts(self):
+    pass
 
-    def runner_on_async_ok(self, host, res, jid):
-        pass
+  def runner_on_async_poll(self, host, res, jid, clock):
+    human_log(res, host)
 
-    def runner_on_async_failed(self, host, res, jid):
-        log(host, 'ASYNC_FAILED', res)
+  def runner_on_async_ok(self, host, res, jid):
+    human_log(res, host)
 
-    def playbook_on_start(self):
-        pass
+  def runner_on_async_failed(self, host, res, jid):
+    human_log(res, host)
 
-    def playbook_on_notify(self, host, handler):
-        pass
+  def playbook_on_start(self):
+    pass
 
-    def playbook_on_no_hosts_matched(self):
-        pass
+  def playbook_on_notify(self, host, handler):
+    pass
 
-    def playbook_on_no_hosts_remaining(self):
-        pass
+  def playbook_on_no_hosts_matched(self):
+    pass
 
-    def playbook_on_task_start(self, name, is_conditional):
-        pass
+  def playbook_on_no_hosts_remaining(self):
+    pass
 
-    def playbook_on_vars_prompt(self, varname, private=True, prompt=None, encrypt=None, confirm=False, salt_size=None, salt=None, default=None):
-        pass
+  def playbook_on_task_start(self, name, is_conditional):
+    pass
 
-    def playbook_on_setup(self):
-        pass
+  def playbook_on_vars_prompt(self, varname, private=True, prompt=None, encrypt=None, confirm=False, salt_size=None, salt=None, default=None):
+    pass
 
-    def playbook_on_import_for_host(self, host, imported_file):
-        log(host, 'IMPORTED', imported_file)
+  def playbook_on_setup(self):
+    pass
 
-    def playbook_on_not_import_for_host(self, host, missing_file):
-        log(host, 'NOTIMPORTED', missing_file)
+  def playbook_on_import_for_host(self, host, imported_file):
+    pass
 
-    def playbook_on_play_start(self, name):
-        pass
+  def playbook_on_not_import_for_host(self, host, missing_file):
+    pass
 
-    def playbook_on_stats(self, stats):
-        pass
+  def playbook_on_play_start(self, pattern):
+    pass
+
+  def playbook_on_stats(self, stats):
+    pass
